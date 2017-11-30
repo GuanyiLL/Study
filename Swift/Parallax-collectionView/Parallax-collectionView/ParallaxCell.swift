9021525efe6e69c8a9334bea8bf6806a6bccf383
yi.guan <yi.guan@99bill.com>
2017-11-30 17:31:23 +0800

    Add ParallaxCell protocol

---
+++ Swift/Parallax-collectionView/Parallax-collectionView/ParallaxCell.swift
@@ -0,0 +1,14 @@
+//
+//  ParallaxCell.swift
+//  Parallax-collectionView
+//
+//  Created by Guanyi on 2017/11/30.
+//  Copyright © 2017年 yiguan. All rights reserved.
+//
+
+import UIKit
+
+protocol ParallaxCell {
+    var container: UIView { get set }
+    var bgImageView: UIImageView { get set }
+}
--- Swift/Parallax-collectionView/Parallax-collectionView/ParallaxCollectionViewCell.swift
+++ Swift/Parallax-collectionView/Parallax-collectionView/ParallaxCollectionViewCell.swift
@@ -8,8 +8,40 @@
 
 import UIKit
 
-class ParallaxCollectionViewCell: UICollectionViewCell {
+class ParallaxCollectionViewCell
+: UICollectionViewCell
+, ParallaxCell {
+    var container: UIView = {
+        let view = UIView()
+        view.clipsToBounds = false
+        return view
+    }()
+    var bgImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .center
+        return imageView
+    }()
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    func commonInit() {
+        self.clipsToBounds = true
+        contentView.addSubview(container)
+        container.addSubview(bgImageView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        container.frame = contentView.bounds
+        bgImageView.frame = container.bounds
+    }
 }
 
 extension ParallaxCollectionViewCell {
