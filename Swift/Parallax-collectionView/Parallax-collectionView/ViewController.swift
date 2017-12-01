bc815ebad19b33259d92a86724d90f2f5fc73079
yi.guan <yi.guan@99bill.com>
2017-12-01 15:28:39 +0800

    make collectionview pagingable

--- Swift/Parallax-collectionView/Parallax-collectionView/ViewController.swift
+++ Swift/Parallax-collectionView/Parallax-collectionView/ViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+struct ItemSize {
+    static let width:CGFloat = 280;
+    static let height:CGFloat = 400;
+}
+
 class ViewController
 : UIViewController
 , UICollectionViewDelegate
@@ -15,22 +20,12 @@ class ViewController
 , UICollectionViewDelegateFlowLayout {
 
     var collectionView: UICollectionView!
+    var layout: UICollectionViewFlowLayout!
     var imgNames: NSArray = ["01","02","03","04"];
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.minimumLineSpacing = 0
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.showsVerticalScrollIndicator = false
-        collectionView.showsHorizontalScrollIndicator = false
-        collectionView.register(ParallaxCollectionViewCell.classForCoder(), forCellWithReuseIdentifier: ParallaxCollectionViewCell.reuseIdentifier)
-        collectionView.isPagingEnabled = true
-        view.addSubview(collectionView)
-        collectionView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        configCollectionView()
     }
     
     override func viewDidLayoutSubviews() {
@@ -49,13 +44,50 @@ class ViewController
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: view.frame.size.height - 64)
+        return CGSize(width: ItemSize.width, height: ItemSize.height)
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         //TODO
         print("Did select item at row:[\(indexPath.row)]")
     }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        print("ScrollView content offset: [\(scrollView.contentOffset.x)] \nvelocity: [\(velocity.x)]\nTargetContentOffset: [\(targetContentOffset.move().x)]")
+        
+        if velocity.x == 0 { return }
+        
+        var targetCell: ParallaxCollectionViewCell?
+        var targetX: CGFloat = 0
+        
+        if velocity.x > 0 {
+            targetCell = collectionView.visibleCells.max { $0.frame.minX < $1.frame.minX } as? ParallaxCollectionViewCell
+        } else {
+            targetCell = collectionView.visibleCells.min { $0.frame.minX < $1.frame.minX } as? ParallaxCollectionViewCell
+        }
+        guard let cell = targetCell else { fatalError() }
+        guard let indexPath = collectionView.indexPath(for: cell) else { fatalError() }
+        targetX = CGFloat(indexPath.row) * (ItemSize.width + 20)
+        targetContentOffset.pointee.x = targetX
+    }
+    
+    func configCollectionView() {
+        layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 20
+        layout.headerReferenceSize = CGSize(width: (UIScreen.main.bounds.width - ItemSize.width) / 2, height: ItemSize.height)
+        layout.footerReferenceSize = CGSize(width: (UIScreen.main.bounds.width - ItemSize.width) / 2, height: ItemSize.height)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(ParallaxCollectionViewCell.classForCoder(), forCellWithReuseIdentifier: ParallaxCollectionViewCell.reuseIdentifier)
+        collectionView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        view.addSubview(collectionView)
+    }
+    
     
 }
 
