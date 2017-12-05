8048efe54221b12d55bdfc18b536175d4eae6fb2
yi.guan <yi.guan@99bill.com>
2017-12-05 23:02:52 +0800

    Add collectionView flowlayout

---
+++ Swift/Parallax-collectionView/Parallax-collectionView/ParallaxFlowLayout.swift
@@ -0,0 +1,18 @@
+//
+//  ParallaxFlowLayout.swift
+//  Parallax-collectionView
+//
+//  Created by Guanyi on 2017/12/5.
+//  Copyright © 2017年 yiguan. All rights reserved.
+//
+
+import UIKit
+
+enum FlowLayoutState {
+    case card
+    case fullView
+}
+
+class ParallaxFlowLayout: UICollectionViewFlowLayout {
+    
+}
