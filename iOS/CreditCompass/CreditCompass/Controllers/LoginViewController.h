b1ac7bd898eff48d3003df8ac7b401f081d4a48d
Ra1n <yguanll@icloud.com>
2018-07-26 22:01:41 +0800

    Add Login Controller

---
+++ iOS/CreditCompass/CreditCompass/Controllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  CreditCompass
+//
+//  Created by ra1n on 2018/7/26.
+//  Copyright © 2018 ra1n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
